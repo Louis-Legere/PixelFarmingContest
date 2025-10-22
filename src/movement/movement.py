ca3aaa8ee3702e49d79b2adc1373399e25a01f6e
Louis-Legere <l.legere@student.fontys.nl>
2025-10-22 15:57:15 +0200

    changed the file slightly

--- src/movement/movement.py
+++ src/movement/movement.py
@@ -8,8 +8,6 @@ AXIS_STATE_CLOSED_LOOP_CONTROL: Final = 8
 
 odrv0 = odrive.find_any()
 
-odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-odrv0.axis0.controller.input_vel = 0
+odrv0.axis0.controller.input_vel = 1
 
-odrv0.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-odrv0.axis1.controller.input_vel = 0
+odrv0.axis1.controller.input_vel = 1
