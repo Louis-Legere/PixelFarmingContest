87ce9907be297c3cff5b360633241a30509be4aa
Louis-Legere <l.legere@student.fontys.nl>
2025-10-27 12:50:19 +0100

    changed comments

--- src/operations/movement/movement.py
+++ src/operations/movement/movement.py
@@ -16,8 +16,8 @@ time.sleep(1)
 
 
 
-def moveForward(velocity): #velocity = 11
-    for i in range(int(velocity * 100 + 1)):  #in case of 1.5 its 16. So the loop goes 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15. And 15 divided by 10 is 1.5 again
+def moveForward(velocity): #maximum 2 numbers after the comma
+    for i in range(int(velocity * 100 + 1)):  #in case of velocity = 1.55 its 156. So the loop goes 0,1,2 ... 155. And 155 divided by 100 is 1.55 again
         odrv0.axis0.controller.input_vel = i / 100
         odrv0.axis1.controller.input_vel = i / 100
         print(i / 100)
