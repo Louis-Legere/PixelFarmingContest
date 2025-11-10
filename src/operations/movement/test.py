02cf56faac631b0906b2ac4daff750022c6ae5dd
Louis-Legere <l.legere@student.fontys.nl>
2025-11-10 16:19:15 +0100

    added loop behaviour

--- src/operations/movement/test.py
+++ src/operations/movement/test.py
@@ -1,6 +1,9 @@
 import time
 
 from src.operations.movement.movement import moveForward, stopMoving, turnLeft, stopTurning, turnRight
+from src.controllers.movementController import set_motor_state_idle, set_motor_state_closed_loop
+
+set_motor_state_closed_loop()
 
 print("moving forward")
 moveForward(0.8)
@@ -9,3 +12,5 @@ time.sleep(2)
 
 print("stopping")
 stopMoving()
+
+set_motor_state_idle()
