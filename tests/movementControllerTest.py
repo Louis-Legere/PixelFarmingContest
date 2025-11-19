a47f4117997c3bead16e0986f0bd9031816aa52d
Louis-Legere <l.legere@student.fontys.nl>
2025-11-19 12:45:56 +0100

    added all tests for controller

--- tests/movementControllerTest.py
+++ tests/movementControllerTest.py
@@ -63,3 +63,107 @@ def test_set_motor_state_idle(mock_connection):
     ]
 
     mock_connection.write.assert_has_calls(expected_calls, any_order=False)
+
+
+@pytest.mark.parametrize("state,expected_calls", [
+    (movementController.DrivingState.STOP, [call.moveForward(1)]),
+    (movementController.DrivingState.FORWARD, []),
+    (movementController.DrivingState.BACKWARD, [call.stopMoving(), call.moveForward(1)]),
+    (movementController.DrivingState.TURN_LEFT, [call.stopTurning(), call.moveForward(1)]),
+    (movementController.DrivingState.TURN_RIGHT, [call.stopTurning(), call.moveForward(1)]),
+])
+@patch("time.sleep", return_value=None)
+@patch("src.controllers.movementController.movement")
+def test_transferToForward(mock_movement, mock_sleep, state, expected_calls):
+    # Arrange
+    movementController.currentDrivingState = state
+
+    # Act
+    movementController.transferToForward(1)
+
+    # Assert
+    actual_calls = mock_movement.mock_calls
+    assert actual_calls == expected_calls
+
+
+@pytest.mark.parametrize("state,expected_calls", [
+    (movementController.DrivingState.STOP, [call.moveBackwards(1)]),
+    (movementController.DrivingState.FORWARD, [call.stopMoving(), call.moveBackwards(1)]),
+    (movementController.DrivingState.BACKWARD, []),
+    (movementController.DrivingState.TURN_LEFT, [call.stopTurning(), call.moveBackwards(1)]),
+    (movementController.DrivingState.TURN_RIGHT, [call.stopTurning(), call.moveBackwards(1)]),
+])
+@patch("time.sleep", return_value=None)
+@patch("src.controllers.movementController.movement")
+def test_transferToBackward(mock_movement, mock_sleep, state, expected_calls):
+    # Arrange
+    movementController.currentDrivingState = state
+
+    # Act
+    movementController.transferToBackward(1)
+
+    # Assert
+    actual_calls = mock_movement.mock_calls
+    assert actual_calls == expected_calls
+
+@pytest.mark.parametrize("state,expected_calls", [
+    (movementController.DrivingState.STOP, [call.turnLeft(20)]),
+    (movementController.DrivingState.FORWARD, [call.turnLeft(20)]),
+    (movementController.DrivingState.BACKWARD, []),
+    (movementController.DrivingState.TURN_LEFT, []),
+    (movementController.DrivingState.TURN_RIGHT, [call.stopTurning(), call.turnLeft(20)]),
+])
+@patch("time.sleep", return_value=None)
+@patch("src.controllers.movementController.movement")
+def test_transferToLeft(mock_movement, mock_sleep, state, expected_calls):
+    # Arrange
+    movementController.currentDrivingState = state
+
+    # Act
+    movementController.transferToLeft(20)
+
+    # Assert
+    actual_calls = mock_movement.mock_calls
+    assert actual_calls == expected_calls
+
+
+@pytest.mark.parametrize("state,expected_calls", [
+    (movementController.DrivingState.STOP, [call.turnRight(20)]),
+    (movementController.DrivingState.FORWARD, [call.turnRight(20)]),
+    (movementController.DrivingState.BACKWARD, []),
+    (movementController.DrivingState.TURN_LEFT, [call.stopTurning(), call.turnRight(20)]),
+    (movementController.DrivingState.TURN_RIGHT, []),
+])
+@patch("time.sleep", return_value=None)
+@patch("src.controllers.movementController.movement")
+def test_transferToRight(mock_movement, mock_sleep, state, expected_calls):
+    # Arrange
+    movementController.currentDrivingState = state
+
+    # Act
+    movementController.transferToRight(20)
+
+    # Assert
+    actual_calls = mock_movement.mock_calls
+    assert actual_calls == expected_calls
+
+
+@pytest.mark.parametrize("state,expected_calls", [
+    (movementController.DrivingState.STOP, []),
+    (movementController.DrivingState.FORWARD, [call.stopMoving()]),
+    (movementController.DrivingState.BACKWARD, [call.stopMoving()]),
+    (movementController.DrivingState.TURN_LEFT, [call.stopMoving()]),
+    (movementController.DrivingState.TURN_RIGHT, [call.stopMoving()]),
+])
+@patch("time.sleep", return_value=None)
+@patch("src.controllers.movementController.movement")
+def test_transferToStop(mock_movement, mock_sleep, state, expected_calls):
+    # Arrange
+    movementController.currentDrivingState = state
+
+    # Act
+    movementController.transferToStop()
+
+    # Assert
+    actual_calls = mock_movement.mock_calls
+    assert actual_calls == expected_calls
