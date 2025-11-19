edb9524a94c894b84461814ad361d45a8ec18832
Louis-Legere <l.legere@student.fontys.nl>
2025-11-19 11:33:31 +0100

    added physical test case

--- src/controllers/temp.py
+++ src/controllers/temp.py
@@ -2,35 +2,14 @@ import time
 
 from src.controllers import movementController
 
-print("moving forward")
-movementController.transferToForward(1.5)
+print("tranisiton to idle")
+movementController.set_motor_state_idle()
 
-time.sleep(3)
+print("transition to closed loop")
+movementController.set_motor_state_closed_loop()
 
-print("turning left")
-movementController.transferToLeft(20)
+print("tranisiton to idle")
+movementController.set_motor_state_idle()
 
-time.sleep(3)
-
-print("moving forward")
-movementController.transferToForward(1.5)
-
-time.sleep(3)
-
-print("turning right")
-movementController.transferToRight(20)
-
-time.sleep(3)
-
-print("moving forward")
-movementController.transferToForward(1.5)
-
-time.sleep(3)
-
-print("moving backwards")
-movementController.transferToBackward(1.5)
-
-time.sleep(3)
-
-print("stopping")
-movementController.transferToStop()
+print("transition to closed loop")
+movementController.set_motor_state_closed_loop()
